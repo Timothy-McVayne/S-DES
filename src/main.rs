298cb9a5dc5ab173ff0ae30e74f5ab4760ae5383
Andrew Smith <asmith20@moreheadstate.edu>
2024-09-06 13:21:39 -0400

    Get the individual bytes from the file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bitvec::vec::BitVec;
+use std::fs::{self, File};
+use std::io::Read;
+use std::io;
+use std::string::String; 
 fn main() {
-    println!("Hello, world!");
+
+    let mut file = File::open("test.txt").expect("Can't open file");
+    let metadata = fs::metadata("test.txt").expect("Can't read metadata"); 
+    
+    let mut buffer = vec![0; metadata.len() as usize];
+    file.read(&mut buffer).expect("buffer overflow");
+
+    let mut i = 0; 
+
+    while i < buffer.len()
+    {
+        let letter = format!("{:08b}", buffer[i]);
+        println!("{}", letter);
+        i += 1;
+    }
+
+    //io::copy(&mut file, &mut vec);
+
+    //let size = BitVec::len(&vec); 
+    //println!("{}", size);
 }
