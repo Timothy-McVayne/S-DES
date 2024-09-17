28e3dcea448f06723fbe140a8c386424af243f64
Andrew Smith <asmith20@moreheadstate.edu>
2024-09-17 16:21:42 -0400

    Finish key generation function, write loop to perform encryption

--- src/functions.rs
+++ src/functions.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::fs::File;
+use std::io::Read; 
 
 const IP: [u8; 8] = [1, 5, 2, 0, 3, 7, 4, 6];
 const KEYORD10: [u8; 10] = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5]; 
@@ -16,41 +18,68 @@ pub fn permute<T: Into<u16>>(value: T, order: &[u8], length: usize) -> u16
     return rearranged;
 }
 
-pub fn generateKeys(path: &str)
+pub fn generate_keys(path: &str) -> (u8, u8)
 {
     //Key at key.txt is 1010000010
     let path = path.trim(); 
     let key = fs::read_to_string(path).expect("Can't read file");
     let parsed_key = u16::from_str_radix(&key, 2).expect("Invalid"); 
-    let permuted = permute(parsed_key, &KEYORD10, KEYORD10.len());
-    println!("{:010b}", permuted);
-    let left: u8 = ((permuted >> 5) & 0b00011111) as u8;
-    let right: u8 = (permuted & 0b00011111) as u8; 
 
-    println!("{:05b}", left);
-    println!("{:05b}", right); 
+    let permuted = permute(parsed_key, &KEYORD10, KEYORD10.len());
+
+    let left: u8 = ((permuted >> 5) & 0b00011111) as u8;
+    let right: u8 = (permuted & 0b00011111) as u8;  
 
     let rotleft = ((left << 1) | (left >> 4)) & 0b00011111;
     let rotright = ((right << 1) | (right >> 4)) & 0b00011111;
-    let combined: u16 = ((rotleft as u16) << 5) | (rotright as u16);
-    println!("Combined before p8 prep: {:010b}", combined);
-    let combined = (combined & 0b0000000011111111);
-    println!("Combined after p8 prep: {:010b}", combined);
 
-    let k1 = permute(combined, &KEYORD8, KEYORD8.len());
- 
-    let rotleft = ((left << 2) | (left >> 4)) & 0b00011111;
-    let rotright = ((right << 2) | (right >> 4)) & 0b00011111;
+    let combined: u16 = ((rotleft as u16) << 5) | (rotright as u16) & 0b0000000011111111;
+    let k1 = permute(combined, &KEYORD8, KEYORD8.len()) as u8;
 
-    println!("Rotleft after a 2bit shift: {:05b}", rotleft);
-    println!("Rotright after a 2bit shift: {:05b}", rotright);
+    let rotleft = ((left << 3) | (left >> 2)) & 0b00011111;
+    let rotright = ((right << 3) | (right >> 2)) & 0b00011111;
 
+    let combined: u16 = (((rotleft as u16) << 5) | (rotright as u16)) & 0b0000000011111111;
+    let k2 = permute(combined, &KEYORD8, KEYORD8.len()) as u8;
+
+    return (k1, k2); 
     //generate keys from 10 bit input
 }
 
-pub fn encrypt()
+pub fn encrypt(plain: &str, key: &str)
 {
-    /*
+    let (k1, k2) = generate_keys(key); 
+
+    let mut file = File::open(plain).expect("Can't open file");
+    let metadata = file.metadata().expect("Can't read metadata");
+    let mut buffer: Vec<u8> = vec![0; metadata.len() as usize];
+    file.read(&mut buffer).expect("Can't read file");
+    let mut cipher_text: Vec<u8> = Vec::new();
+    for bytes in &buffer
+    {
+        cipher_text.push(*bytes);
+    }
+
+    for element in &mut cipher_text
+    {
+        encrypt_loop(*element, k1, k2); 
+    }
+    //fs::write("cipher.txt", cipher_text).unwrap();
+}
+
+pub fn decrypt()
+{
+    
+}
+
+fn encrypt_loop(data: u8, k1: u8, k2: u8)
+{
+
+    println!("data: {:08b}", data);
+    println!("K1: {:08b}", k1); 
+    println!("K2: {:08b}", k2); 
+
+        /*
     Initial permutation FUNCTION
 
     Expanded permutation converts 4 bit input to 8 bit output
@@ -64,9 +93,4 @@ pub fn encrypt()
     inverse of IP
 
     */
-}
-
-pub fn decrypt()
-{
-    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 mod functions; 
 
-use std::fs;
-use std::fs::File;
-use std::io::{stdout, Read};
-use std::io::{self, Write};
-
-const IP: [u8; 8] = [1, 5, 2, 0, 3, 7, 4, 6];
-const KEYORD: [u8; 10] = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5]; 
-
-use functions::{generateKeys, permute};
+use std::io; 
+use std::io::Write;
+use functions::{generate_keys, encrypt};
 fn main() {
 
     /*
@@ -23,49 +17,5 @@ fn main() {
     println!("Please enter the name of the file with the key: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut path).expect("Couldn't read line");
-    generateKeys(&mut path); 
-
-    /*let mut file = File::open("test.txt").expect("Can't open file");
-    let metadata = file.metadata().expect("Can't read metadata");
-    let mut buffer: Vec<u8> = vec![0; metadata.len() as usize];
-    file.read(&mut buffer).expect("Can't read file");
-    let mut cipher_text: Vec<u8> = Vec::new();
-    //let mut arr: [u8; 8] = [1, 5, 2, 0, 3, 7, 4, 6];
-    for bytes in &buffer
-    {
-        cipher_text.push(*bytes);
-        print!("{}", bytes); 
-    }
-
-    println!(""); 
-
-    if cipher_text == buffer
-    {
-        println!("Equal");
-        fs::write("cipher.txt", cipher_text).unwrap();
-    }
-    */
-    /*println!("Plaintext is: {:08b}", value);  
-
-    for i in 0..8 
-    {
-        rearranged |= ((value >> (8 - arr[i] - 1)) & 1) << ((arr.len() - 1) - i);
-    };
-
-    println!("Ciphertext after IP is: {:08b}", rearranged);*/
-
-    //functions::print_hello(); 
-    
-    /*let mut i = 0; 
-    while i < buffer.len()
-    {
-        let letter = format!("{:08b}", buffer[i]);
-        println!("{}", letter);
-        i += 1;
-    }*/
-
-    //io::copy(&mut file, &mut vec);
-
-    //let size = BitVec::len(&vec); 
-    //println!("{}", size);
+    encrypt("test.txt", "key.txt");
 }
